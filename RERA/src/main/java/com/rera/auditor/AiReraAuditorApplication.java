148ae617086f88576c378c326dc7667f345a41cd
1111khajju1111 <sriramkhadhyoth123@gmail.com>
2026-07-26 16:11:57 +0530

    semma

--- RERA/src/main/java/com/rera/auditor/AiReraAuditorApplication.java
+++ RERA/src/main/java/com/rera/auditor/AiReraAuditorApplication.java
@@ -11,9 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class AiReraAuditorApplication {
     public static void main(String[] args) {
 
-        
-		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-		dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
+       
 		
 
         SpringApplication.run(AiReraAuditorApplication.class, args);
